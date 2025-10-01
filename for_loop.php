ef9b4e39046239251ce523960af80769c45f465e
“Abdul <abdulmajeedkhattak1@gmail.com>
2025-10-01 12:13:31 +0500

    its for_loop file

---
+++ for_loop.php
@@ -0,0 +1,35 @@
+<!-- For Loop in Php -->
+
+<?php
+for ($x = 0; $x <= 10; $x++) {
+    if ($x ==3){
+        echo "Sana Ullah<br>";
+        break;
+    }
+    echo "I am Number $x of for loop <br>";
+}
+?>
+
+
+<?php
+$x = 0;
+while ($x <= 10) {
+    echo "i am while looop Number $x <br>";
+    $x++;
+}
+
+?>
+
+<?php
+$x = 0;
+do{
+    echo "I am Do looop number $x <br>";
+    $x++;
+}
+while($x<=10);
+?>
+
+
+
+
+
