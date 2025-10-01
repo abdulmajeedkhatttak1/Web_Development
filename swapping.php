f450ee84fd300b9c0b6ed7a72eed9a354f9ae405
“Abdul <abdulmajeedkhattak1@gmail.com>
2025-10-01 12:15:29 +0500

    its swapping  file

---
+++ swapping.php
@@ -0,0 +1,14 @@
+<!-- //////////////////////////////////// SWAPPING  /////////////////////// -->
+<?php
+$number1 = 10;
+$number2 = 20;
+
+$no = $number1;
+$number1 = $number2;
+$number2 = $no;
+echo $number1;
+echo "<br>";
+echo $number2;
+
+
+?>
