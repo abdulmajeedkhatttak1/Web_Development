16b340a28c1165df6da7303b5052fe375b00fe2b
“Abdul <abdulmajeedkhattak1@gmail.com>
2025-10-07 16:00:42 +0500

    Server Location

---
+++ php/server_location.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // echo $_SERVER["DOCUMENT_ROOT"];
+    // echo "<br>";
+    // echo $_SERVER["SERVER_NAME"];
+    // echo "<br>";
+    // echo $_SERVER["SERVER_NAME"];
+    // echo "<br>";
+    // echo $_SERVER["REQUEST_METHOD"];
+    // echo $_GET['name'];
+    // $_SESSION['name'] = "Sana Ullah";
+    // echo $_SESSION['name'];
+    // $_GET[""];
+    // $_SERVER[""];
+    // $_POST[""];
+    // $_REQUEST[""];
+    // $_FILES[""];
+    // $_SESSION[""];
+    // $_COOKIE[""];
+    // $_ENV[""];
+    echo phpversion();
+
+    ?>
+</body>
+</html>
